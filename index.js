fba99a14310ea18adbc869c629c8ed21cce86512
formatted <ronivegh@gmail.com>
2016-08-31 18:35:23 -0600

    node + express server up and running locally

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+var port = process.env.PORT || '8080';
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(port, function () {
+  console.log('Example app listening on port 8080!');
+});
