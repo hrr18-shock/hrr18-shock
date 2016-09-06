d1e80847dfb27fd607fd76e71c6c633b2f57825c
Caleb Aston <caleb.k.aston@gmail.com>
2016-09-06 09:25:56 -0600

    Made our page and ui links work.

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ var path = require('path');
 
 var port = process.env.PORT || '8080';
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+app.use(express.static(__dirname));
+
+// During my MVP, an HIR told me I should always use static when creating a Single Page App.
+
+// original code Roni wrote
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/index.html'));
+// });
 
 app.listen(port, function () {
   console.log('Example app listening on port 8080!');
