955d5afa4a4ab2ab92294c1e7f91fbf6bd195915
formatted <ronivegh@gmail.com>
2016-09-06 09:44:10 -0600

    grunt added w/ jshint

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    nodemon: {
+      dev: {
+        script: 'index.js'
+      }
+    },
+
+    jshint: {
+      files: [
+        '*.js'
+      ],
+      options: {
+        force: false,
+        reporterOutput: '',
+        jshintrc: '.jshintrc'
+        // ignores: [
+        //   'public/lib/**/*.js',
+        //   'public/dist/**/*.js'
+        // ]
+      }
+    }
+
+  });
+
+  ///// grunt.loadNpmTasks('nameOfPackage');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-nodemon');
+
+
+  ///// main grunt tasks
+
+  grunt.registerTask('build', [
+    'jshint'
+  ]);
+
+};
