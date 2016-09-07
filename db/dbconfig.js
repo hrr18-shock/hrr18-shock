bd4ceacdbd9732d16fa8a0438b6e9ba28e672e6c
ItsDavidThai <itsdavidthai@gmail.com>
2016-09-07 15:51:54 -0700

    create schema with postgres and sequelize

--- db/dbconfig.js
+++ db/dbconfig.js
@@ -3,14 +3,20 @@
 npm install sequelize -S
 npm install pg pg-hstore -S
 */
-
+// start connection to the sql server
 var Sequelize = require('sequelize');
-var db = new Sequelize('shock', '', '',{
-  dialect:'postgres',
+var dbURL = process.env.DATABASE_URL === undefined ? 'shock' : process.env.DATABASE_URL;
+db = new Sequelize(dbURL, '', '', {
+  logging: false,
+  dialect:'postgres'
+  // dialectOptions: {
+  //   ssl: true  for SSL config since Heroku gives you this out of the box
+  // }
 });
 
+// Define user models for each table
 exports.Users = db.define('users', {
-  id            : { type: Sequelize.INTEGER, primaryKey: true},
+  id            : { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
   first_name    : Sequelize.STRING,
   last_name     : Sequelize.STRING,
   username      : Sequelize.STRING,
@@ -21,15 +27,17 @@ exports.Users = db.define('users', {
   updatedAt     : Sequelize.DATE
 });
 
-exports.Trainers = db.define('users', {
-  id            : { type: Sequelize.INTEGER, primaryKey: true},
-  id_user       : { type: Sequelize.INTEGER, references : {model: exports.Users, key:exports.Users.id} }
+exports.Trainers = db.define('trainers', {
+  id            : { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
+  id_user       : { type: Sequelize.INTEGER, references : {model: exports.Users, key:exports.Users.id} },
+  createdAt     : Sequelize.DATE,
+  updatedAt     : Sequelize.DATE
 });
 
 
 exports.Exercises = db.define('exercises', {
 
-  id            :{ type: Sequelize.INTEGER, primaryKey: true},
+  id            :{ type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
   name          :Sequelize.STRING,
   type          :Sequelize.STRING,
   description   :Sequelize.STRING,
@@ -38,18 +46,22 @@ exports.Exercises = db.define('exercises', {
 
 exports.Workout_list = db.define('workout_list', {
 
-  id            :{ type: Sequelize.INTEGER, primaryKey: true},
+  id            :{ type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
   date_created  :Sequelize.DATE,
   comments      :Sequelize.STRING,
   id_user       :{ type: Sequelize.INTEGER, references : {model: exports.Users, key:exports.Users.id} },
-  id_trainer    :{ type: Sequelize.INTEGER, references : {model: exports.Trainers, key:exports.Trainers.id} }
+  id_trainer    :{ type: Sequelize.INTEGER, references : {model: exports.Trainers, key:exports.Trainers.id} },
+  createdAt     : Sequelize.DATE,
+  updatedAt     : Sequelize.DATE
 
 })
 exports.Workouts = db.define('workouts', {
-  id              :{ type: Sequelize.INTEGER, primaryKey: true},
+  id              :{ type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
   id_workout_list :{ type: Sequelize.INTEGER, references : {model: exports.Workout_list, key:exports.Workout_list.id} },
   comments        :Sequelize.STRING,
-  id_exercises    :{ type: Sequelize.INTEGER, references : {model: exports.Exercises, key:exports.Exercises.id} }
+  id_exercises    :{ type: Sequelize.INTEGER, references : {model: exports.Exercises, key:exports.Exercises.id} },
+  createdAt     : Sequelize.DATE,
+  updatedAt     : Sequelize.DATE
 })
 
 // link relationships in place of junction table
@@ -58,16 +70,17 @@ exports.Trainers.belongsToMany(exports.Users, {through:'Trainer_client'});
 
 
 
-exports.Users.create({first_name:'david', last_name:'dave', username:'helloworld,', address:'1234 hello world', phone_number:3214443333, email:'helloworld@gmail.com'})
-
-
-
-
-// creates a connection to a preexisiting schema
-// exports.Users.sync().then(function () {
-//   console.log('users synced!')
-// });
-
+//TODO: add rest of table syncs
+// creates the table if no table has been created yet
+exports.Users.sync().then(function () {
+  console.log('users synced!')
+});
 /*exports.Trainers.sync().then(function () {
   console.log('trainers synced!')
 });*/
+
+
+
+// testing things past here can be ignored
+// exports.Users.create({first_name:'david', last_name:'dave', username:'helloworld', address:'1234 hello world', phone_number:3214443333, email:'helloworld@gmail.com'})
+
