a65f65ea6588d715b93da31718b5f34fe7a53c24
ItsDavidThai <itsdavidthai@gmail.com>
2016-09-07 20:05:54 -0700

    move sequelize modes to a new file and set up configuration for heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require('path');
 
 var port = process.env.PORT || '8080';
-
+var db = require('./db/models')
 app.use(express.static('app'));
 
 // During my MVP, an HIR told me I should always use static when creating a Single Page App.
@@ -12,7 +12,12 @@ app.use(express.static('app'));
 // app.get('/', function(req, res) {
 //     res.sendFile(path.join(__dirname + '/index.html'));
 // });
-
+// Sync models with database then run server
+db.sequelize.sync().then(function() {
+  http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+  });
+});
 app.listen(port, function () {
   console.log('Example app listening on port 8080!');
 });
