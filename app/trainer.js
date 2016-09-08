4aefbcf47c1c984c6b4cd2b06b20e5876c994671
Wes Young <wes.young14@gmail.com>
2016-09-08 14:41:39 -0600

    Added functionality to workout form

--- app/trainer.js
+++ app/trainer.js
@@ -34,6 +34,19 @@ app.controller('TrainerCtrl', [
       $scope.clients.push({name: $scope.name, workouts: []});
       $scope.name = '';
     };
+
+    $scope.createWorkout = function(){
+
+      $scope.clientSelect.workouts.push({e1: $scope.e1, d1: $scope.d1, e2: $scope.e2, d2: $scope.d2, e3: $scope.e3, d3: $scope.d3, e4: $scope.e4, d4: $scope.d4, e5: $scope.e5, d5: $scope.d5, e6: $scope.e6, d6: $scope.d6, e7: $scope.e7, d7: $scope.d7, e8: $scope.e8, d8: $scope.d8, e9: $scope.e9, d9: $scope.d9, e10: $scope.e10, d10: $scope.d10 });
+      var index;
+      $scope.clients.forEach(function(item, i){
+        if(item.name === $scope.clientSelect.name){
+          index = i;
+        }
+      });
+      $scope.clients[index] = $scope.clientSelect;
+      console.log($scope.clients[index])
+    }
     // video hardcoded
     $scope.linkS = ['https://www.youtube.com/watch?v=IODxDxX7oi4', 'https://www.youtube.com/watch?v=lQRsYgRafA8&feature=youtu.be', 'https://www.youtube.com/watch?v=Dy28eq2PjcM', 'https://www.youtube.com/watch?v=-4qRntuXBSc', 'https://www.youtube.com/watch?v=U4BS9EXvfyg&index=5&list=PL_UAXxDwtUkFzbr1npphK6WH4ytgJcQS8', 'https://www.youtube.com/watch?v=5a6bRnvjlgg&index=6&list=PL_UAXxDwtUkFzbr1npphK6WH4ytgJcQS8', 'https://www.youtube.com/watch?v=-phbNTs-SwU&index=7&list=PL_UAXxDwtUkFzbr1npphK6WH4ytgJcQS8'];
     $scope.link = 'https://www.youtube.com/watch?v=IODxDxX7oi4';
